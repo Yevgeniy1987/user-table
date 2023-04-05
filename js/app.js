8f6494f466f8b090f7c1647769486dd7f86e2f00
Eugene <evgeniy.lyashko.87@gmail.com>
2023-04-05 10:33:11 -0500

    create-searching-form-and-fix-the-sorting-functions
    -created form in HTML
    -added stylization in CSS;
    -fixed mistakes in options' names in HTML code;
    -added and changed the sorting functions in js;

--- js/app.js
+++ js/app.js
@@ -3,12 +3,11 @@
 const usersTableElem = document.getElementById("table-data");
 const sortSelect = document.getElementById("sort-select");
 
-Users.forEach((user) => {
-  const userTableHTML = createUsersTable(user);
-  usersTableElem.insertAdjacentHTML("beforeend", userTableHTML);
-});
+reRender(usersTableElem, Users, true);
 
 function createUsersTable(user) {
+
+  
   return `<tr class="font-medium">
     <td class="table-text">${user.name}</td>
     <td class="table-text">${user.username}</td>
@@ -24,10 +23,36 @@ function createUsersTable(user) {
 }
 
 sortSelect.addEventListener('change', (event) => {
-  const sortSelection = event.target.value.split("/");
-  console.log(sortSelection);
-  
+  const sortSelection = event.target.value.split('/');
+
+  const key = sortSelection[0];
+  const order = sortSelection[1];
+
+  console.log(key, order);
+
+  smartSort(Users, order, key);
+
+  reRender(usersTableElem, Users, true);
 });
 
+function reRender(to, array, clear) {
+  if (clear) {
+    to.innerHTML = '';
+  }
+  array.forEach((user) => {
+    const userTableHTML = createUsersTable(user);
+    to.insertAdjacentHTML('beforeend', userTableHTML);
+  });
+}
 
+function smartSort(array, order = 1, key) {
+  return array.sort((a, b) => {
+    const elem1 = key ? a[key] : a;
+    const elem2 = key ? b[key] : b;
 
+    return (
+      String(elem1).localeCompare(String(elem2), undefined, { numeric: true }) *
+      order
+    );
+  });
+}
